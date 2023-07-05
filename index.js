2ffc851d803010cf5d58ca05b63570f32b1c787d
Eduard Ursu <eduardursu78@gmail.com>
2023-07-05 21:15:44 +0200

    Improvements

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const fs = require("fs");
-const data_icons = require('./data_icons.json')
+const data_icons = require("./data_icons.json");
 const path = require("path");
-/*
-
-"triangle-exclamation": {
-    xmlFile: "incognito_splash",
-    color:"#DADCDF",
-    type:"solid",
-    nopadding: true
-  },
-*/
 
 const defaultColor = "@android:color/black";
 const targetPath = "drawable/";
 const sourcePath = "./node_modules/@fortawesome/fontawesome-pro/svgs/";
 const padding = 8;
+const assetsFolderPath = "./chrome/"; // Specify the path to the image folder
+const buildLogPath = "build_log.log"; // Specify the path for the image log file
+// Helper function to check if a file is an image file
+function isImageFile(file) {
+  const imageExtensions = [".jpg", ".jpeg", ".png", ".gif"]; // Add more extensions if needed
+  const ext = path.extname(file).toLowerCase();
+  return imageExtensions.includes(ext);
+}
+// Helper function to recursively search for images in a folder
+function findImagesForIcon(iconName, folderPath) {
+  let imagePaths = [];
+  const files = fs.readdirSync(folderPath);
+
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      // Pass the icon name and the full path to the recursive call
+      imagePaths = imagePaths.concat(findImagesForIcon(iconName, filePath));
+    } else if (stat.isFile() && isImageFile(file) && file.includes(iconName)) {
+      imagePaths.push(filePath);
+    }
+  });
+
+  return imagePaths;
+}
+let count = 0
+let icount = 0
+
 function generateXml(icon, svgContent, xmlFilePath) {
   const pathRegex = /<path[^>]+d="([^"]+)"/;
   const pathMatch = svgContent.match(pathRegex);
   const viewBoxRegex = /viewBox="([^"]+)"\s*/;
-  const viewBoxMatch = svgContent.match(viewBoxRegex); 
-  const viewBoxValues = viewBoxMatch[1].split(' ');
- 
+  const viewBoxMatch = svgContent.match(viewBoxRegex);
+  const viewBoxValues = viewBoxMatch[1].split(" ");
+
   const viewBoxWidth = viewBoxValues[2];
   const viewBoxHeight = viewBoxValues[3];
   console.log(`------ ${pathMatch} ------`);
@@ -32,24 +53,22 @@ function generateXml(icon, svgContent, xmlFilePath) {
 
   const rawpath = pathMatch[1];
   // Replace occurrences of '-.' with '-0.' in the path data
-  const path = rawpath.replace(/([^0-9])\./g, '$10.');
-  const padding = ((viewBoxHeight/ 100)  * 16)
-  const vb = 512
+  const path = rawpath.replace(/([^0-9])\./g, "$10.");
+  //const padding = ((viewBoxHeight/ 100)  * 16)
+  //const vb = 512
   let paddedViewBox = 675.84;
+  let swidth = parseInt((paddedViewBox - viewBoxWidth) / 2);
+  let sheight = parseInt((paddedViewBox - viewBoxHeight) / 2);
+  let color = data_icons[icon].color;
 
-  let swidth = parseInt((paddedViewBox - viewBoxWidth ) / 2);
-  let sheight = parseInt(( paddedViewBox - viewBoxHeight) / 2 );
-  let color = data_icons[icon].color
-  if( !color )
-    color=defaultColor
-  let dp = data_icons[icon].dp
-  if( !dp )
-      dp="24"
-  let nopadding = data_icons[icon].nopadding
-  if(nopadding){
-    paddedViewBox ="512"
-    swidth="0"
-    sheight="0"
+  if (!color) color = defaultColor;
+  let dp = data_icons[icon].dp;
+  if (!dp) dp = "24";
+  let nopadding = data_icons[icon].nopadding;
+  if (nopadding) {
+    paddedViewBox = "512";
+    swidth = "0";
+    sheight = "0";
   }
 
   const xmlContent = `<?xml version="1.0" encoding="utf-8"?>
@@ -70,16 +89,31 @@ function generateXml(icon, svgContent, xmlFilePath) {
 </vector>`;
 
   fs.writeFileSync(xmlFilePath, xmlContent);
+  icount++
   console.log(`Generated: ${xmlFilePath}`);
+
+  // Log the replaced icon names and spotted files
+  const logEntry = `Icon '${icon}' replaced by '${xmlFilePath}', spotted files:\n`;
+  const imagePaths = findImagesForIcon(icon, assetsFolderPath);
+console.log(imagePaths); 
+  const spottedFiles = imagePaths.filter((imagePath) =>
+    imagePath.includes(icon)
+  );
+
+  if (spottedFiles.length > 0) {
+    count+=spottedFiles.length
+    const spottedFilesLog = spottedFiles.join("\n");
+    const logContent = `${logEntry}${spottedFilesLog}\n\n`;
+    fs.appendFileSync(buildLogPath, logContent);
+  }
 }
 
 for (const icon in data_icons) {
   const i = data_icons[icon];
-  let type = i.type
-  if(!type)
-    type="light"
-  const xmlFilePath = targetPath+icon + ".xml";
-  const svgPath = sourcePath +type+"/"+ `${i['icon-name']}.svg`;
+  let type = i.type;
+  if (!type) type = "light";
+  const xmlFilePath = targetPath + icon + ".xml";
+  const svgPath = sourcePath + type + "/" + `${i["icon-name"]}.svg`;
   console.log(`------ ${icon} ------`);
   console.log(`Processing: ${svgPath}`);
 
@@ -92,3 +126,5 @@ for (const icon in data_icons) {
 
   generateXml(icon, svgContent, xmlFilePath);
 }
+const time = new Date()
+fs.appendFileSync(buildLogPath, `\n------------------------------------\n${time}\nCount of spotted files : ${count}\nNumber of xml icons generated : ${icount}\n------------------------------------`);
---
+++ scripts/clean_images.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+const buildLogPath = "build.log"; // Specify the path for the image log file
+const cleanLogPath = "clean.log"; // Specify the path for the deleted images log file
+let count = 0;
+// Read the image log file and delete the tracked images
+function deleteTrackedImages() {
+  const imageLogContent = fs.readFileSync(buildLogPath, "utf-8");
+  const logEntries = imageLogContent.split("\n\n");
+
+  logEntries.forEach((logEntry) => {
+    const lines = logEntry.trim().split("\n");
+    const replacedIcon = lines[0].match(/Icon '(.+)' replaced/)[1];
+    const spottedFiles = lines.slice(1);
+
+    spottedFiles.forEach((spottedFile) => {
+      const imagePath = spottedFile.trim();
+      if (fs.existsSync(imagePath)) {
+        fs.unlinkSync(imagePath);
+        console.log(`Deleted: ${imagePath}`);
+        fs.appendFileSync(cleanLogPath, `${replacedIcon}: ${imagePath}\n`);
+        count++;
+      }
+    });
+  });
+  const time = new Date();
+  fs.appendFileSync(
+    buildLogPath,
+    `------------------------------------\n${time}\nNumber of deleted files : ${count}\n------------------------------------`
+  );
+}
+
+// Call the function to delete the tracked images
+deleteTrackedImages();
